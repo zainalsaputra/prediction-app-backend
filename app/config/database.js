1fedbd629a5d9bce858c9a9164d82a73943ddb35
Zainal Saputra <101336276+zainalsaputra@users.noreply.github.com>
2025-02-28 00:12:23 +0700

    [fix] bug setup sec-production #6

--- app/config/database.js
+++ app/config/database.js
@@ -1,2 +1,2 @@
-const config = require('../config/config')[process.env.NODE_ENV || 'development'];
+const config = require('../config/config')[process.env.NODE_ENV || 'production'];
 module.exports = config;
