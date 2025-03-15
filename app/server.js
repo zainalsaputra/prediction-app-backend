731fe09828a4cf4414c9951d4697562b6dd9f00d
Zainal Saputra <101336276+zainalsaputra@users.noreply.github.com>
2025-03-16 02:46:33 +0700

    [fix] setup third-production #2

--- app/server.js
+++ app/server.js
@@ -21,6 +21,10 @@ app.use(cors({
   allowedHeaders: ["Content-Type", "Authorization"]
 }));
 
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
